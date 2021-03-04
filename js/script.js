1e5bc26c1601921ad668afca993f4e5507c6ba32
solov <solovejko00@mail.ru>
2021-03-04 20:27:49 +0300

    del city v1.0

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+let globalId = 7;
+
+function getId() {
+    return globalId++;
+}
 
 async function f() {
     let myKey = '0b5edc7455a336d544760ce639198bc9';
@@ -20,34 +25,29 @@ function addNewCity(city='Moscow', temperature=5, img='weather.png',
                     humidity=52, x=59.88, y=30.42) {
     let list = document.querySelector('.favorites');
     let newFavorite = document.createElement('li');
-    newFavorite.getAttribute('class');
     newFavorite.setAttribute('class', 'favorite');
+    newFavorite.setAttribute('id', 'id' + getId().toString());
     let newH3 = document.createElement('h3');
     newH3.textContent = city;
     let newSpan = document.createElement('span');
-    newSpan.getAttribute('class');
     newSpan.setAttribute('class', 'temperature');
     newSpan.textContent = temperature.toString()+'°C';
     let newImg = document.createElement('img');
-    newImg.getAttribute('src');
     newImg.setAttribute('src', 'images/' + img);
     let newButton = document.createElement('button');
-    newButton.getAttribute('type');
-    newButton.getAttribute('class');
     newButton.setAttribute('type', 'button');
     newButton.setAttribute('class', 'delete');
+    newButton.setAttribute('onclick', "del('" + newFavorite.getAttribute('id') + "')");
     // нужно будет добавить onclick
+
     let newUl = document.createElement('ul');
-    newUl.getAttribute('class');
     newUl.setAttribute('class', 'weather');
 
     let newLi1 = document.createElement('li');
     let newSpanBold1 = document.createElement('span');
-    newSpanBold1.getAttribute('class');
     newSpanBold1.setAttribute('class', 'bold');
     newSpanBold1.textContent = 'Ветер';
     let newSpanNormal1 = document.createElement('span');
-    newSpanNormal1.getAttribute('class');
     newSpanNormal1.setAttribute('class', 'normal');
     newSpanNormal1.textContent = wind.toString() + ' m/s';
     newLi1.appendChild(newSpanBold1);
@@ -55,11 +55,9 @@ function addNewCity(city='Moscow', temperature=5, img='weather.png',
 
     let newLi2 = document.createElement('li');
     let newSpanBold2 = document.createElement('span');
-    newSpanBold2.getAttribute('class');
     newSpanBold2.setAttribute('class', 'bold');
     newSpanBold2.textContent = 'Облачность';
     let newSpanNormal2 = document.createElement('span');
-    newSpanNormal2.getAttribute('class');
     newSpanNormal2.setAttribute('class', 'normal');
     newSpanNormal2.textContent = cloud;
     newLi2.appendChild(newSpanBold2);
@@ -67,11 +65,9 @@ function addNewCity(city='Moscow', temperature=5, img='weather.png',
 
     let newLi3 = document.createElement('li');
     let newSpanBold3 = document.createElement('span');
-    newSpanBold3.getAttribute('class');
     newSpanBold3.setAttribute('class', 'bold');
     newSpanBold3.textContent = 'Давление';
     let newSpanNormal3 = document.createElement('span');
-    newSpanNormal3.getAttribute('class');
     newSpanNormal3.setAttribute('class', 'normal');
     newSpanNormal3.textContent = pressure.toString() + ' hpa';
     newLi3.appendChild(newSpanBold3);
@@ -79,11 +75,9 @@ function addNewCity(city='Moscow', temperature=5, img='weather.png',
 
     let newLi4 = document.createElement('li');
     let newSpanBold4 = document.createElement('span');
-    newSpanBold4.getAttribute('class');
     newSpanBold4.setAttribute('class', 'bold');
     newSpanBold4.textContent = 'Влажность';
     let newSpanNormal4 = document.createElement('span');
-    newSpanNormal4.getAttribute('class');
     newSpanNormal4.setAttribute('class', 'normal');
     newSpanNormal4.textContent = humidity.toString() + '%';
     newLi4.appendChild(newSpanBold4);
@@ -91,11 +85,9 @@ function addNewCity(city='Moscow', temperature=5, img='weather.png',
 
     let newLi5 = document.createElement('li');
     let newSpanBold5 = document.createElement('span');
-    newSpanBold5.getAttribute('class');
     newSpanBold5.setAttribute('class', 'bold');
     newSpanBold5.textContent = 'Координаты';
     let newSpanNormal5 = document.createElement('span');
-    newSpanNormal5.getAttribute('class');
     newSpanNormal5.setAttribute('class', 'normal');
     newSpanNormal5.textContent = '[' + x.toString() + ', ' + y.toString() +']';
     newLi5.appendChild(newSpanBold5);
@@ -116,4 +108,11 @@ function addNewCity(city='Moscow', temperature=5, img='weather.png',
     list.appendChild(newFavorite);
 
     console.log(list);
+}
+
+function del(idCity) {
+    //idCity = 'id1';
+    console.log(document.getElementById(idCity).style.display);
+    document.getElementById(idCity).style.display = "none";
+    console.log(document.getElementById(idCity).style.display);
 }
