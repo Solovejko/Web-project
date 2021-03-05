24d283abaa5461df4ef32258811dbac899e5bad9
solov <solovejko00@mail.ru>
2021-03-05 10:18:53 +0300

    add load start

--- js/script.js
+++ js/script.js
@@ -1,24 +1,44 @@
-let globalId = 7;
+if (localStorage.getItem('globalId') == null)
+    localStorage.setItem('globalId', '0');
+
+loadFavoriteCity();
 
 function getId() {
     return globalId++;
 }
 
-async function addNewCity() {
+function loadFavoriteCity() {
+    for (let i = 0; i < Number.parseInt(localStorage.getItem('globalId')); i++)
+        addNewCity(localStorage.getItem('id' + i.toString()), true, 'id' + i.toString());
+}
+
+async function addNewCity(nameCity = undefined, load=false, id='id-1') {
+    console.log(nameCity, ' ', id);
+
+    if (nameCity === null)
+        return;
+
     let myKey = '0b5edc7455a336d544760ce639198bc9';
     let input_city = document.getElementById('add_city');
-    let nameCity = input_city.value;
-    input_city.value = '';
+
+    if (!load){
+        nameCity = input_city.value;
+        input_city.value = '';
+    }
 
     if (nameCity === "")
         return;
 
-    console.log(nameCity);
-
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${nameCity}&appid=${myKey}&units=metric&lang=ru`;
+
+    if (load)
+    {
+        createEmptyElement(nameCity, id);
+    }
 
     let response = await fetch(url);
     let commits = await response.json();
+
     console.log(commits);
 
     if (commits.cod === "401"){
@@ -36,12 +56,13 @@ async function addNewCity() {
         return;
     }
 
-    console.log(~~commits.main.temp);
-    console.log(commits.wind.speed);
-    console.log(commits.weather[0].description);
-    console.log(commits.main.pressure);
-    console.log(commits.main.humidity);
-    console.log(commits.coord.lon.toFixed(1), ' ', commits.coord.lat.toFixed(1));
+    if (!load){
+        let l = localStorage.getItem('globalId');
+        l = Number.parseInt(l);
+        localStorage.setItem('id' + l, nameCity);
+        localStorage.setItem('globalId', (l + 1).toString());
+        id = 'id' + l;
+    }
 
     let temp = ~~commits.main.temp;
     let img = commits.weather[0].icon + '.png';
@@ -52,17 +73,17 @@ async function addNewCity() {
     let x = commits.coord.lon.toFixed(1);
     let y = commits.coord.lat.toFixed(1);
 
-    createNewElement(nameCity, temp, img, wind, cloud, press, hum, x, y);
+    createNewElement(nameCity, temp, img, wind, cloud, press, hum, x, y, id);
 }
 
 
 function createNewElement(city='Moscow', temperature=5, img='weather.png',
                     wind=6.0, cloud='Сloudy', pressure=1013,
-                    humidity=52, x=59.88, y=30.42) {
+                    humidity=52, x=59.88, y=30.42, id='id-1') {
     let list = document.querySelector('.favorites');
     let newFavorite = document.createElement('li');
     newFavorite.setAttribute('class', 'favorite');
-    newFavorite.setAttribute('id', 'id' + getId().toString());
+    newFavorite.setAttribute('id', id);
     let newH3 = document.createElement('h3');
     newH3.textContent = city;
     let newSpan = document.createElement('span');
@@ -145,8 +166,93 @@ function createNewElement(city='Moscow', temperature=5, img='weather.png',
     console.log(list);
 }
 
+function createEmptyElement(city='Moscow', id='id-1') {
+    let list = document.querySelector('.favorites');
+
+    let newFavorite = document.createElement('li');
+    newFavorite.setAttribute('class', 'favorite');
+    newFavorite.setAttribute('id', id);
+    let newH3 = document.createElement('h3');
+    newH3.textContent = city;
+
+    let newSpan = document.createElement('span');
+    newSpan.setAttribute('class', 'temperature');
+    let newImg = document.createElement('img');
+    newImg.setAttribute('src', 'images/unknown.png');
+    let newButton = document.createElement('button');
+    newButton.setAttribute('type', 'button');
+    newButton.setAttribute('class', 'delete');
+    newButton.setAttribute('onclick', "del('" + newFavorite.getAttribute('id') + "')");
+
+    let newUl = document.createElement('ul');
+    newUl.setAttribute('class', 'weather');
+
+    let newLi1 = document.createElement('li');
+    let newSpanBold1 = document.createElement('span');
+    newSpanBold1.setAttribute('class', 'bold');
+
+    let newSpanNormal1 = document.createElement('span');
+    newSpanNormal1.setAttribute('class', 'normal');
+
+    newLi1.appendChild(newSpanBold1);
+    newLi1.appendChild(newSpanNormal1);
+
+    let newLi2 = document.createElement('li');
+    let newSpanBold2 = document.createElement('span');
+    newSpanBold2.setAttribute('class', 'bold');
+
+    let newSpanNormal2 = document.createElement('span');
+    newSpanNormal2.setAttribute('class', 'normal');
+
+    newLi2.appendChild(newSpanBold2);
+    newLi2.appendChild(newSpanNormal2);
+
+    let newLi3 = document.createElement('li');
+    let newSpanBold3 = document.createElement('span');
+    newSpanBold3.setAttribute('class', 'bold');
+
+    let newSpanNormal3 = document.createElement('span');
+    newSpanNormal3.setAttribute('class', 'normal');
+
+    newLi3.appendChild(newSpanBold3);
+    newLi3.appendChild(newSpanNormal3);
+
+    let newLi4 = document.createElement('li');
+    let newSpanBold4 = document.createElement('span');
+    newSpanBold4.setAttribute('class', 'bold');
+
+    let newSpanNormal4 = document.createElement('span');
+    newSpanNormal4.setAttribute('class', 'normal');
+
+    newLi4.appendChild(newSpanBold4);
+    newLi4.appendChild(newSpanNormal4);
+
+    let newLi5 = document.createElement('li');
+    let newSpanBold5 = document.createElement('span');
+    newSpanBold5.setAttribute('class', 'bold');
+
+    let newSpanNormal5 = document.createElement('span');
+
+    newLi5.appendChild(newSpanBold5);
+    newLi5.appendChild(newSpanNormal5);
+
+    newUl.appendChild(newLi1);
+    newUl.appendChild(newLi2);
+    newUl.appendChild(newLi3);
+    newUl.appendChild(newLi4);
+    newUl.appendChild(newLi5);
+
+    newFavorite.appendChild(newH3);
+    newFavorite.appendChild(newSpan);
+    newFavorite.appendChild(newImg);
+    newFavorite.appendChild(newButton);
+    newFavorite.appendChild(newUl);
+
+    list.appendChild(newFavorite);
+}
+
 function del(idCity) {
     console.log(document.getElementById(idCity).style.display);
     document.getElementById(idCity).style.display = "none";
-    console.log(document.getElementById(idCity).style.display);
+    localStorage.removeItem(idCity);
 }
